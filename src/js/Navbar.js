bb757e1003adb555bb7f8eb977f666f9f43a9544
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-23 18:06:29 +0200

    added fixed top navbar from bootstrap

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -2,5 +2,17 @@ import React from "react";
 import "../css/Navbar.css";
 
 export default function Navbar() {
-  return <div className="navbar">Navigation bar content goes here.</div>;
+  return (
+    <div className="navbar">
+      <nav className="navbar fixed-top bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            {/*start content */}
+
+            {/* end content */}
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
