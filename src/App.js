8d1c50d5098eaded3c3fad3273e49588f3770c07
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-23 17:50:19 +0200

    added t Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./js/Navbar";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header">
+        <Navbar />
+      </header>
     </div>
   );
 }
---
+++ src/js/Navbar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../css/Navbar.css";
+
+export default function Navbar() {
+  return <div className="navbar">Navigation bar content goes here.</div>;
+}
