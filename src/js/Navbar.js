01342f70550442ad8855c444134f832cac296f03
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-24 10:13:41 +0200

    adjusted  height of navbar && added the navbar div container

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -11,34 +11,36 @@ let navbarArray = [
 ];
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg colorNavbar">
-      <div className="container-fluid">
-        {/* here needs to be my image */}
-        <a className="navbar-brand" href="#">
-          {navbarArray[0].img}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            {/*  make forloop */}
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-          </ul>
+    <div className="navBar">
+      <nav className="navbar navbar-expand-lg colorNavbar">
+        <div className="container-fluid">
+          {/* here needs to be my image */}
+          <a className="navbar-brand" href="#">
+            {navbarArray[0].img}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {/*  make forloop */}
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
