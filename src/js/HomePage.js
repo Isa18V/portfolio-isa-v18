e7c48223213322a1f1e1b28efda939ea16eec0d2
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-25 17:58:28 +0200

    made array of objects called content

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -2,6 +2,15 @@ import React from "react";
 import "../css/HomePage.css";
 import isa from "../img/isa.jpg";
 
+const homePageArrays = [
+  { content: "Hello, it`s me Isabel Voican" },
+  { content: "I`m a Front-End developer" },
+  {
+    content:
+      "I'm excited to share a bit about myself with you. Kindness, learning, and staying motivated are the core pillars of my journey. With a heart that overflows with compassion, I strive to make a positive impact in the lives of those around me.",
+  },
+  { content: "Download CV" },
+];
 export default function HomePage() {
   return (
     <div className="HomePage" id="home">
