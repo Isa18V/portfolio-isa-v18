7115c5bbdc2c5b41418c76aa0bb7b48e0fae435c
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-25 18:23:21 +0200

    styled homepage

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -22,7 +22,22 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+      {/* start 2 column */}
+      <div className="col-md-6 homeContent">
+        <h2>Hello, it`s me</h2>
+        <h2>Isabel Voican</h2>
+        <h3>
+          I`m a <span className="beige">Front-End developer</span>
+        </h3>
+        <p>
+          I'm excited to share a bit about myself with you. Kindness, learning,
+          and staying motivated are the core pillars of my journey. With a heart
+          that overflows with compassion, I strive to make a positive impact in
+          the lives of those around me.
+        </p>
 
+        <button className="buttonHome buttonHome1">Download Cv</button>
+      </div>
       {/* end row */}
     </div>
   );
