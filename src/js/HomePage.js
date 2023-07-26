c4ce1cff8bb4eb8448f9b2a69ccff5dad8bfc438
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-26 10:57:02 +0200

    styled and made home page responsive

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/HomePage.css";
-import isa from "../img/isa.jpg";
+import isa from "../img/isa.jpeg";
 
 const homePageArrays = [
   {
@@ -18,24 +18,26 @@ export default function HomePage() {
     <div className="HomePage" id="home">
       {/* start row */}
       <div className="row">
-        <div className="col-md-6 homeContent ">
-          <div className="isaImageContainer">
-            <img className="isaImage" src={isa} alt="imgisa" />
+        <div className="col-md-6 homeContent">
+          <div className="isaImageContainer ">
+            <img className="isaImage " src={isa} alt="imgisa" />
           </div>
         </div>
-      </div>
-      {/* start 2 column */}
-      <div className="col-md-6 homeContent">
-        <h2>{homePageArrays[0].title}</h2>
-        <h2>{homePageArrays[0].name}</h2>
-        <h3>
-          I`m a <span className="beige">Front-End developer</span>
-        </h3>
-        <p>{homePageArrays[0].content}</p>
 
-        <button className="buttonHome buttonHome1">
-          {homePageArrays[0].download}
-        </button>
+        {/* start 2 column */}
+        <div className="col-md-6 homeContent">
+          <h2>{homePageArrays[0].title}</h2>
+          <h2>{homePageArrays[0].name}</h2>
+          <h3>
+            I`m a <span className="beige">Front-End developer</span>
+          </h3>
+          <p>{homePageArrays[0].content}</p>
+          <div className="buttonContainer">
+            <button className="buttonHome buttonHome1">
+              {homePageArrays[0].download}
+            </button>
+          </div>
+        </div>
       </div>
       {/* end row */}
     </div>
