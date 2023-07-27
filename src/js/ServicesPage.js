4c19e986af7408dd42dd0d78f3e983ec99f16f7a
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-27 15:15:32 +0200

    deleted card in service page
    i want to make other cards here

--- src/js/ServicesPage.js
+++ src/js/ServicesPage.js
@@ -4,38 +4,8 @@ import "../css/ServicesPage.css";
 
 export default function ServicesPage() {
   return (
-    <div className="ServicesPage" id="services">
-      <div class="container scroll-1">
-        <div class="card">
-          <div class="card__image">
-            <img src={brasov}></img>
-          </div>
-          <div class="card__content">
-            <span class="card__title">Hello</span>
-            <p class="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card__image"></div>
-          <div class="card__content">
-            <span class="card__title">Hello</span>
-            <p class="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card__image"></div>
-          <div class="card__content">
-            <span class="card__title">Hello</span>
-            <p class="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div classNameName="ServicesPage" id="services">
+      hello
     </div>
   );
 }
