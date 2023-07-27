cb8fd6e1b48c5cf2dce70d4a732536dadc658f1c
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-27 16:44:28 +0200

    deleted the cards

--- src/js/ServicesPage.js
+++ src/js/ServicesPage.js
@@ -1,39 +1,12 @@
 import React from "react";
+import brasov from "../img/brasov.jpg";
 
 import "../css/ServicesPage.css";
 
 export default function ServicesPage() {
   return (
     <div className="ServicesPage" id="services">
-      <div className="container scroll-1">
-        <div className="card">
-          <div className="card__image"></div>
-          <div className="card__content">
-            <span className="card__title">Hello</span>
-            <p className="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card__image"></div>
-          <div className="card__content">
-            <span className="card__title">Hello</span>
-            <p className="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card__image"></div>
-          <div className="card__content">
-            <span className="card__title">Hello</span>
-            <p className="card__describe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-      </div>
+      helo
     </div>
   );
 }
