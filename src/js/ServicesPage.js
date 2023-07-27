9ad115610b3e87a915c34bce81d8c8b94776f02f
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-27 15:05:34 +0200

    made cards in services page and also scrolls

--- src/js/ServicesPage.js
+++ src/js/ServicesPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+import brasov from "../img/brasov.jpg";
 import "../css/ServicesPage.css";
 
 export default function ServicesPage() {
   return (
     <div className="ServicesPage" id="services">
-      hello
+      <div class="container scroll-1">
+        <div class="card">
+          <div class="card__image">
+            <img src={brasov}></img>
+          </div>
+          <div class="card__content">
+            <span class="card__title">Hello</span>
+            <p class="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card__image"></div>
+          <div class="card__content">
+            <span class="card__title">Hello</span>
+            <p class="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card__image"></div>
+          <div class="card__content">
+            <span class="card__title">Hello</span>
+            <p class="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
