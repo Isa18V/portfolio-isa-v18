d128fbd39a18f26c44b6a05962e9485bc6e691bb
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-27 15:48:21 +0200

    added scaffold of the cards to my servicepage

--- src/js/ServicesPage.js
+++ src/js/ServicesPage.js
@@ -4,8 +4,36 @@ import "../css/ServicesPage.css";
 
 export default function ServicesPage() {
   return (
-    <div classNameName="ServicesPage" id="services">
-      hello
+    <div className="ServicesPage" id="services">
+      <div className="container scroll-1">
+        <div className="card">
+          <div className="card__image"></div>
+          <div className="card__content">
+            <span className="card__title">Hello</span>
+            <p className="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__image"></div>
+          <div className="card__content">
+            <span className="card__title">Hello</span>
+            <p className="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__image"></div>
+          <div className="card__content">
+            <span className="card__title">Hello</span>
+            <p className="card__describe">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
