33d4fa17f198a95d24857cd741c03059d8f2b04d
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-29 11:03:10 +0200

    made scaffold of contact Page&& styled

--- src/js/ContactPage.js
+++ src/js/ContactPage.js
@@ -3,8 +3,49 @@ import "../css/ContactPage.css";
 
 export default function ContactPage() {
   return (
-    <div className="ContactPage" id="contact">
-      hello
+    <div className="contact-page" id="contact">
+      <h1 className="contact-page__heading">Contact Me</h1>
+      <p className="contact-page__description">
+        If you have any questions or inquiries, feel free to contact me using
+        the form below:
+      </p>
+      <form className="contact-page__form">
+        <label htmlFor="name" className="contact-page__label">
+          Name:
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="contact-page__input"
+          required
+        />
+
+        <label htmlFor="email" className="contact-page__label">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="contact-page__input"
+          required
+        />
+
+        <label htmlFor="message" className="contact-page__label">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="contact-page__textarea"
+          required
+        ></textarea>
+
+        <button type="submit" className="contact-page__button">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
